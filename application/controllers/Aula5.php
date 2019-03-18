9865c871c187f7510f4b772f5a5374188e15a6b6
DiegoNascimento1 <diegosoaresnascimento1@gmail.com>
2019-03-18 10:14:31 -0300

    Criando um Controller e Carregando uma View

---
+++ application/controllers/Aula5.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Aula5 extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('aula5_view');
+	}
+}
---
+++ application/views/aula5_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Criando views</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <h1>Trabalhando com views</h1>
+    <p>Hoje estamos aprendendo a como trabalhar com Views.</p>
+</body>
+</html>
